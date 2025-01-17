232749218b912783b6fa02789a73b37bed86756d
cmeKyLe <khant.khantminthaw2021@gmail.com>
2025-01-16 18:41:30 -0800

    entire homepage, authentication and database setup is integrated

--- reading-den/app/layout.tsx
+++ reading-den/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
 import localFont from 'next/font/local';
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react"
+import { auth } from "@/auth";
 
 const ibmPlexSans = localFont({
   src : [
@@ -26,15 +28,21 @@ export const metadata: Metadata = {
   description: "ReadingDen is a book borrowing univerisity library management website",
 };
 
-const RootLayout = ({children,}: {children: ReactNode;}) =>{
+const RootLayout = async ({children,}: {children: ReactNode;}) =>{
+  const session = await auth();
+  
   return (
     <html lang="en">
+      <SessionProvider session= {session}>
       <body
         className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
+
+
         <Toaster />
       </body>
+      </SessionProvider>
     </html>
   );
 }
--- reading-den/components/Header.tsx
+++ reading-den/components/Header.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { cn } from '@/lib/utils';
+import { Avatar, AvatarFallback} from "@/components/ui/avatar"
+import { cn, getInitials } from '@/lib/utils';
+import { Session } from "next-auth";
 import Image from 'next/image';
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 
-const Header = () => {
+const Header = ({session} : {session : Session} ) => {
     const pathname = usePathname();
   return (
     <header className="my-10 flex justify-between gap-5">
@@ -15,6 +17,15 @@ const Header = () => {
         <ul className="flex flex-row items-center gap-8">
             <li>
                 <Link href="/library" className={cn('text-base cursor-pointer capitalize', pathname === '/library' ? 'text-light-200' : 'text-light-100')}>Library</Link>
+            </li>
+            <li>
+              <Link href="/my-profile">
+                <Avatar>
+                  <AvatarFallback className= "bg-amber-100">
+                    {getInitials(session?.user?.name || 'IN') }
+                  </AvatarFallback>
+                </Avatar>
+              </Link>
             </li>
         </ul>
     </header>
